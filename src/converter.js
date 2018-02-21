5600b64c3a88ab44d9225d081f81333062750bce
Diego Speroni <diegosperoni1@gmail.com>
2018-02-21 16:34:29 -0300

    gitignore and stuff

--- src/converter.js
+++ src/converter.js
@@ -1,11 +1,11 @@
 function converter (num, opt) {
-    if (opt == 1) {
-      return num * 2.20462
-    }else if (opt == 0) {
-      return num / 2.20462
-    }else{
-      return "Error"
-    }
+  if (opt == 1) {
+    return num * 2.20462
+  }else if (opt == 0) {
+    return num / 2.20462
+  }else{
+    return "Error"
+  }
 } 
 
 exports.default = converter
